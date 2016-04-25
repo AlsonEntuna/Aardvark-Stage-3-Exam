a738cf6490bdfd64baad98b2565edb40b8dddb14
John Alson Entuna <johnalson_entuna@outlook.com>
2016-04-25 17:20:00 +0800

    [+] Player Health and Player Movement Ground Work

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float MaxHealth;
+
+    private float currentHealth;
+
+    public bool IsAlive
+    {
+        get
+        {
+            return currentHealth > 0;
+        }
+    }
+
+    void Start()
+    {
+        currentHealth = MaxHealth;
+    }
+
+    void Update()
+    {
+        currentHealth = Mathf.Clamp(currentHealth, 0, MaxHealth);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+    }
+}
